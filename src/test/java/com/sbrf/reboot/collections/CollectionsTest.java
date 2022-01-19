64b170a348e5e6a806def6960c2a97df6b2a3471
shemiak <89091691341@mail.ru>
2022-01-19 18:11:21 +0300

    Realized addStudentToRating(), addMoneyToBox() and addBookToShelf(). Also realized extra method addBookToCase()

--- src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
+++ src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
@@ -2,7 +2,7 @@ package com.sbrf.reboot.collections;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,14 +24,18 @@ public class CollectionsTest {
      * Вопрос.
      * Какую коллекцию из Collections framework вы предпочтете для текущего хранения и использования списка студентов?
      *
+     ОТВЕТ: LinkedList так как при добалении в начало в данной коллекции минимльная сложность по времени
+     *
      * Проинициализируйте students, добавьте в нее 4 фамилии студентов что бы тест завершился успешно.
      */
     @Test
     public void addStudentToRating() {
+        List<String> students = new LinkedList<>();
 
-        List<String> students = null;
-
-        //...
+        students.add("Козлов");
+        students.add("Иванов");
+        students.add("Петров");
+        students.add("Сидоров");
 
         assertEquals(4, students.size());
     }
@@ -44,14 +48,26 @@ public class CollectionsTest {
      * Вопрос.
      * Какую коллекцию из Collections framework вы предпочтете использовать для хранения монет в боксе.
      *
+     ОТВЕТ: HashSet, так как используются уникальные монеты и при этом не имеет значения, как именно друг относительно
+     друга они будут расположены
+     *
      * Проинициализируйте moneyBox, добавьте в нее 10 монет что бы тест завершился успешно.
      */
     @Test
     public void addMoneyToBox() {
 
-        List<Integer> moneyBox = null;
+        Set<Integer> moneyBox = new HashSet<>();
 
-        //...
+        moneyBox.add(1);
+        moneyBox.add(2);
+        moneyBox.add(3);
+        moneyBox.add(4);
+        moneyBox.add(5);
+        moneyBox.add(6);
+        moneyBox.add(7);
+        moneyBox.add(8);
+        moneyBox.add(9);
+        moneyBox.add(10);
 
         assertEquals(10, moneyBox.size());
     }
@@ -64,6 +80,8 @@ public class CollectionsTest {
      * Вопрос.
      * Какую коллекцию из Collections framework вы предпочтете использовать для хранения книг.
      *
+     ОТВЕТ: ArrayList, так как мы знаем, отуда берем книгу и куда потом ставим
+     *
      * Проинициализируйте bookshelf, добавьте в нее 3 книги что бы тест завершился успешно.
      */
     @Test
@@ -71,12 +89,50 @@ public class CollectionsTest {
         class Book {
         }
 
-        List<Book> bookshelf = null;
+        List<Book> bookshelf = new ArrayList<>();
 
-        //...
+        bookshelf.add(new Book());
+        bookshelf.add(new Book());
+        bookshelf.add(new Book());
 
         assertEquals(3, bookshelf.size());
     }
 
+    /*
+     * Задача на 5+.
+     * Имеется книжная полка.
+     * Вы подходите к шкафу и хотите взять конкретную книгу конкретного автора при условии, что авторы уникальны, а
+     * названия нет (разные авторы могут написать книги с одинаковым названием) так же, у одного автора книг может быть
+     * несколько, но один автор не может написать несколько книг с одинаковым названием
+     *
+     * Вопрос.
+     * Какой интерфейс из Collections framework вы предпочтете использовать для хранения книг.
+     *
+     ОТВЕТ: HashMap, так как у нас есть необходимость поиска книг по автору уникальному автору и названию
+     *
+     * Проинициализируйте bookcase, добавьте в него 2х авторов и пронициализируйте booksByTolstoy и booksByLondon. В
+     * переменную booksByTolstoy необходимо добавить 2 книги, а в переменную booksByLondon - одну, чтобы тесты
+     * завершились корректно
+     */
 
+    @Test
+    public void addBookToSCase() {
+        class Book {
+        }
+
+        Map<String, HashMap<String, Book>> bookcase = new HashMap<>();
+        HashMap<String, Book> booksByTolstoy = new HashMap<>();
+        HashMap<String, Book> booksByLondon = new HashMap<>();
+
+        booksByTolstoy.put("Анна Каренина", new Book());
+        booksByTolstoy.put("Война и мир", new Book());
+        booksByLondon.put("Белый клык", new Book());
+
+        bookcase.put("Лев Толстой", booksByTolstoy);
+        bookcase.put("Джек Лондон", booksByLondon);
+
+        assertEquals(2, bookcase.size());
+        assertEquals(1, booksByLondon.size());
+        assertEquals(2, booksByTolstoy.size());
+    }
 }
