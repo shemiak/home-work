90e2b2bfb26df13f2002a118a960949908ae9859
shemiak <89091691341@mail.ru>
2022-02-11 04:31:17 +0300

    Реализована работа с базой данных. Создана таблица CUSTOMER с полями id, name, eMail. В классе CustomerH2Repository реализованы два метода для работы с этой таблицей createCustomer и getAll.

--- src/main/java/com/sbrf/reboot/repository/impl/CustomerH2Repository.java
+++ src/main/java/com/sbrf/reboot/repository/impl/CustomerH2Repository.java
@@ -2,36 +2,51 @@ package com.sbrf.reboot.repository.impl;
 
 import com.sbrf.reboot.dto.Customer;
 import com.sbrf.reboot.repository.CustomerRepository;
+import lombok.NonNull;
 
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class CustomerH2Repository implements CustomerRepository {
 
     private final String JDBC_DRIVER = "org.h2.Driver";
-    private final String DB_URL = "jdbc:h2:~/my_db";
+    private final String DB_URL = "jdbc:h2:~/test";
 
     private final String USER = "sa";
     private final String PASS = "";
 
-    public CustomerH2Repository() {
-        Connection conn = null;
-        Statement stmt = null;
+    private static Connection conn;
+    private Statement stmt;
 
+    public CustomerH2Repository() throws SQLException, ClassNotFoundException {
+        Class.forName(JDBC_DRIVER);
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        stmt = conn.createStatement();
     }
 
     @Override
-    public List<Customer> getAll() {
+    public boolean createCustomer(@NonNull String userName, String eMail) throws SQLException {
+        stmt.execute("INSERT INTO CUSTOMER(name, eMail) VALUES(" + "'" + userName + "','" + eMail + "')");
+        return true;
+    }
+
+    @Override
+    public List<Customer> getAll() throws SQLException {
         List<Customer> customers = new ArrayList<>();
 
-        return customers;
-    }
+        stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM CUSTOMER");
+        while (rs.next()) {
+            Customer customer = new Customer();
+            customer.setId(rs.getLong("id"));
+            customer.setName(rs.getString("name"));
+            customer.setEMail(rs.getString("eMail"));
 
-    @Override
-    public boolean createCustomer(String name, String eMail) {
-        return false;
+            customers.add(customer);
+        }
+        return customers;
     }
 }
 
